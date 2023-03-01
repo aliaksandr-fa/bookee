16adeab2f16f61e4f1551dd65bc2fe9b5ee7400f
Aliaksandr Falei <aliaksandr.fa.mi.re.do@gmail.com>
2023-03-01 22:03:57 +0100

    Updated ci/cd scripts

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,4 +8,6 @@ apt-get update -yqq
 apt-get install git -yqq
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/bin --filename=composer --quiet
+ls -la
 composer install --no-dev --no-scripts --prefer-dist --optimize-autoloader --ignore-platform-reqs
+
